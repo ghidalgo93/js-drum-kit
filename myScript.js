05f19526f0e751e42afd7e33456c1b9185ea4a77
Gerardo Hidalgo-Cuellar <ghidalgo93@gmail.com>
2020-09-18 09:27:47 -0600

    finished adding in js

--- myScript.js
+++ myScript.js
@@ -1,7 +1,19 @@
 // JavaScript for Drum Kit app
 
+function playSound(e){
+	const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`); 
+	const key = document.querySelector(`.key[data-key="${e.keyCode}"]`); 
+	if(!audio) return; //stop function from running all together
+	audio.currentTime = 0; //rewind to start
+	audio.play();
+	key.classList.add('playing');
+}
 
-window.addEventListener("keydown", function(e){
-	//console.log(e.keyCode);
-	const audio = document.querySelector 
-})
+function removeTransition(e){
+	if(e.propertyName !== 'transform') return // skip if not a transform
+	this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener("keydown", playSound);
